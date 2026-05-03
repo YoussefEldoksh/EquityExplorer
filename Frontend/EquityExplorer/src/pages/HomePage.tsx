648500b30b11538c816f60b8c56114ac0f218ca2
ahmed <a.samehpsn@gmail.com>
2026-05-04 02:31:03 +0300

    changed name of file invest suggestions

--- Frontend/EquityExplorer/src/pages/HomePage.tsx
+++ Frontend/EquityExplorer/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/HeroSection';
 import FeaturesSection from '../components/FeaturesSection';
 import ToolsSection from '../components/ToolsSection';
 import Footer from '../components/Footer';
-import InvestSuggestions from '../components/InvestSuggestions';
+import InvestSuggestions from '../components/InvestSuggestionsSection';
 function HomePage() {
   return (
     <>
