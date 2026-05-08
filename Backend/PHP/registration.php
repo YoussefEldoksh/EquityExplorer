526b641c68433329ea8b7a76144be4707d83adad
youssefaymanelsersy <es-yousef.ayman2027@alexu.edu.eg>
2026-05-08 19:12:29 +0300

    fix(network): support mobile/local network access and add env/driver checks

--- Backend/PHP/registration.php
+++ Backend/PHP/registration.php
@@ -2,7 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 // CORS Headers (Required for React)
-header("Access-Control-Allow-Origin: http://localhost:5173"); 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
