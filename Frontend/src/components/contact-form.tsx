bac81d263ac74d719fe4ffc96b6a462c3f5e63ee
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2026-05-08 20:12:09 +0300

    added watchlist button;

--- Frontend/src/components/contact-form.tsx
+++ Frontend/src/components/contact-form.tsx
@@ -37,6 +37,7 @@ export function ContactForm({
   };
 
   return (
+    <>
     <div className={cn('w-5/6 flex flex-col gap-6', className)} {...props}>
       <Card className="rounded-lg overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
@@ -112,5 +113,6 @@ export function ContactForm({
         </CardContent>
       </Card>
     </div>
+    </>
   );
 }
