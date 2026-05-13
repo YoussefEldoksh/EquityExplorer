e6db1cbcfe46b7d189451b83aaebade45fc6c0a1
youssefaymanelsersy <es-yousef.ayman2027@alexu.edu.eg>
2026-05-13 03:46:21 +0300

    fix: final removal of manual CORS headers from remaining PHP scripts

--- Backend/PHP/logout.php
+++ Backend/PHP/logout.php
@@ -1,18 +1,5 @@
 <?php
-//CORS Headers
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
-
-//Handle Preflight
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
 
 // Destroy the Session
 session_start();
--- Backend/PHP/registration.php
+++ Backend/PHP/registration.php
@@ -2,18 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
 
 session_start();
 require_once "db_connection.php";
