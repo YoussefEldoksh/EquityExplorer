be458d05284ee0bf7ddb56294408b9ee3f26c40e
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2026-05-13 05:08:36 +0300

    etfadal

--- Frontend/src/components/FeaturesSection.tsx
+++ Frontend/src/components/FeaturesSection.tsx
@@ -3,29 +3,28 @@ import BorderGlow from './BorderGlow';
 import { TrendingUp, DollarSign, Scale } from 'lucide-react';
 function FeaturesSection() {
   return (
-    <div className="px-8 py-16  ">
+    <div className="px-8 py-16 overflow-hidden ">
       <h2 className="text-4xl font-bold mb-12 text-gray-900 ">
         Why Invest in stock market
       </h2>
 
-      <div className="grid md:grid-cols-3  gap-4  w-full max-w-7xl mx-auto  ">
-        <BorderGlow
-          borderRadius={20}
-          glowRadius={40}
-          glowIntensity={1}
-          glowColor="40 80 80"
-          backgroundColor="#120F17"
-          colors={['#c084fc', '#f472b6', '#38bdf8']}
-        >
-          <div className="p-6 h-full text-white ">
-            <TrendingUp className="text-teal-400 w-8 h-8 mb-4 " />
-            <h3 className="text-xl font-semibold mb-3">Long-Term Growth</h3>
-            <p className="text-gray-400 text-sm">
-              Stocks have historically delivered higher returns than savings,
-              helping your wealth grow over time.
-            </p>
-          </div>
-        </BorderGlow>
+      <div className="grid md:grid-cols-3 gap-4 w-full max-w-7xl mx-auto min-w-0">        <BorderGlow
+        borderRadius={20}
+        glowRadius={40}
+        glowIntensity={1}
+        glowColor="40 80 80"
+        backgroundColor="#120F17"
+        colors={['#c084fc', '#f472b6', '#38bdf8']}
+      >
+        <div className="p-6 h-full text-white ">
+          <TrendingUp className="text-teal-400 w-8 h-8 mb-4 " />
+          <h3 className="text-xl font-semibold mb-3">Long-Term Growth</h3>
+          <p className="text-gray-400 text-sm">
+            Stocks have historically delivered higher returns than savings,
+            helping your wealth grow over time.
+          </p>
+        </div>
+      </BorderGlow>
 
         <BorderGlow
           borderRadius={20}
