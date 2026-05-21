7e93d733805366bae766058ecff12802f18891df
youssefaymanelsersy <es-yousef.ayman2027@alexu.edu.eg>
2026-05-21 05:35:38 +0300

    fix: remove lingering conflict markers in update_password.php

--- Backend/PHP/update_password.php
+++ Backend/PHP/update_password.php
@@ -1,23 +1,6 @@
 <?php
 // PHP script to set or update user password
-<<<<<<< HEAD
 header('Content-Type: application/json');
-
-=======
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
->>>>>>> be78f61 (Fix Auth, Dependencies, and implement Password Management for OAuth users)
 require_once "db_connection.php";
 require_once "auth_middleware.php";
 
